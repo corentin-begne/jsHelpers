c381d29310adf242efbcc8882cb06d74a87787c7
corentin begne <corentin.begne@gmail.com>
2016-03-13 03:03:31 +0100

    more

---
+++ socket.js
@@ -0,0 +1,114 @@
+/*global B, extendSingleton, getSingleton, UserHelper, isDefined, VoiceHelper */
+var SocketHelper;
+(function(){
+    "use strict";
+    /**
+    * @name SocketHelper
+    * @description To define socket events
+    * @property {Object} [_socket = B.bClient._socket] Client socket instance
+    * @constructor
+    */
+    SocketHelper = function(){
+        require['bower_components/socket.io-1.4.5'];
+        extendSingleton(SocketHelper);
+        this.token;
+        this.events = {
+            auth:{
+                name: "auth.acknowledge",
+                cb: this.connected.bind(this)
+            },
+            connectError:{
+                name: "connect_error",
+                cb: this.connectError.bind(this)
+            },
+            disconnected:{
+                name: "disconnect",
+                cb: this.disconnected.bind(this)
+            }
+        };
+    };
+
+    /**
+     * @member SocketHelper#getInstance
+     * @description get the single class instance
+     * @return {SocketHelper} the single class instance
+     */
+    SocketHelper.getInstance = function(){
+        return getSingleton(SocketHelper);
+    };
+
+    /**
+     * @method SocketHelper#init
+     * @param  {Object} events Object containing all events to be set
+     * @description Initialize socket events
+     */
+    SocketHelper.prototype.init = function(events, socket){
+        var that = this;
+        if(!isDefined(socket)){
+            socket = this.socket;
+        }
+        if (isDefined(this._socket) && isDefined(events)) {
+            $.each(events, addEvent);
+        }
+
+        $(window).unbind("beforeunload");
+        $(window).bind("beforeunload", disconnect);
+
+        /**
+         * @event SocketHelper#disconnect
+         * @description Disconnect user socket on beforeunload event
+         */
+        function disconnect(event) {                  
+            if (isDefined(that._socket)){
+                that._socket.disconnect();         
+            }
+        }
+
+        /**
+         * @description Add socket event
+         * @method SocketHelper#addEvent
+         * @private
+         * @param  {String}   [event] Socket event name
+         * @param  {Function} [cb]    Callback of the event
+         */
+        function addEvent(name, params){
+            that._socket.on(params.name, params.cb);
+        }
+    };
+
+    /**
+     * @description Send data by event to RTS
+     * @method SocketHelper#send
+     * @param  {String} [event] event to use
+     * @param  {Object} [data]  data to send
+     */
+    SocketHelper.prototype.send = function(event, data){
+        data = isDefined(data) ? data : {};
+        data.token = this.token;
+        this._socket.emit(event, data);
+    };
+
+    SocketHelper.prototype.connect = function(user, host, port){
+        console.log("connecting to socket server ...");
+        this.socket = io("http://" + host + ":" + port, {
+            reconnection: false, 
+            autoConnect: false,
+            query: $.param(user)
+        });
+        this.init(this.events, this.socket); 
+        this.socket.connect(); 
+    };
+
+    SocketHelper.prototype.connected = function(data){
+        console.log("connected to socket server", data);
+    };
+
+    SocketHelper.prototype.disconnected = function(){
+        console.error("disconnected from socket server");
+    };
+
+    SocketHelper.prototype.connectError = function(){
+        console.error("can't connect to socket server");
+    };
+
+})();
