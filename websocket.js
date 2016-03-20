cae8555aba9b8dab34f2698c364d84f7174a02cf
corentin begne <corentin.begne@gmail.com>
2016-03-20 17:55:36 +0100

    more

--- websocket.js
+++ websocket.js
@@ -1,15 +1,15 @@
 /*global B, extendSingleton, getSingleton, UserHelper, isDefined, VoiceHelper */
-var SocketHelper;
+var WebsocketHelper;
 (function(){
     "use strict";
     /**
-    * @name SocketHelper
+    * @name WebsocketHelper
     * @description To define socket events
     * @property {Object} [_socket = B.bClient._socket] Client socket instance
     * @constructor
     */
-    SocketHelper = function(cb){      
-        extendSingleton(SocketHelper);
+    WebsocketHelper = function(cb){      
+        extendSingleton(WebsocketHelper);
         this.token;
         this.events = {
             onopen: this.connected.bind(this),
@@ -22,24 +22,24 @@ var SocketHelper;
     };
 
     /**
-     * @member SocketHelper#getInstance
+     * @member WebsocketHelper#getInstance
      * @description get the single class instance
-     * @return {SocketHelper} the single class instance
+     * @return {WebsocketHelper} the single class instance
      */
-    SocketHelper.getInstance = function(cb){
+    WebsocketHelper.getInstance = function(cb){
         if(isDefined(cb)){
-            getSingleton(SocketHelper, cb);
+            getSingleton(WebsocketHelper, cb);
         } else {
-            return getSingleton(SocketHelper);
+            return getSingleton(WebsocketHelper);
         }
     };
 
     /**
-     * @method SocketHelper#init
+     * @method WebsocketHelper#init
      * @param  {Object} events Object containing all events to be set
      * @description Initialize socket events
      */
-    SocketHelper.prototype.init = function(events){
+    WebsocketHelper.prototype.init = function(events){
         var that = this;
         
         $.each(events, addEvent);
@@ -48,7 +48,7 @@ var SocketHelper;
         $(window).bind("beforeunload", disconnect);
 
         /**
-         * @event SocketHelper#disconnect
+         * @event WebsocketHelper#disconnect
          * @description Disconnect user socket on beforeunload event
          */
         function disconnect(event) {                  
@@ -59,7 +59,7 @@ var SocketHelper;
 
         /**
          * @description Add socket event
-         * @method SocketHelper#addEvent
+         * @method WebsocketHelper#addEvent
          * @private
          * @param  {String}   [event] Socket event name
          * @param  {Function} [cb]    Callback of the event
@@ -71,11 +71,11 @@ var SocketHelper;
 
     /**
      * @description Send data by event to RTS
-     * @method SocketHelper#send
+     * @method WebsocketHelper#send
      * @param  {String} [event] event to use
      * @param  {Object} [data]  data to send
      */
-    SocketHelper.prototype.send = function(event, data){
+    WebsocketHelper.prototype.send = function(event, data){
         data = isDefined(data) ? data : {};
         data.token = this.token;        
         this.socket.send({
@@ -84,7 +84,7 @@ var SocketHelper;
         });
     };
 
-    SocketHelper.prototype.connect = function(url){
+    WebsocketHelper.prototype.connect = function(url){
         console.log("connecting to socket server ...");
         try{
             this.socket = WebSocket(url);
@@ -94,7 +94,7 @@ var SocketHelper;
         }
     };
 
-    SocketHelper.prototype.recv = function(msg){
+    WebsocketHelper.prototype.recv = function(msg){
         if(!isDefined(this.userEvents[msg.type])){
             console.error("socket event received but not defined", msg);
             return false;
@@ -102,15 +102,15 @@ var SocketHelper;
         this.userEvents[msg.type](msg.data);
     };
 
-    SocketHelper.prototype.connected = function(msg){
+    WebsocketHelper.prototype.connected = function(msg){
         console.log("connected to socket server", msg);
     };
 
-    SocketHelper.prototype.disconnected = function(){
+    WebsocketHelper.prototype.disconnected = function(){
         console.error("disconnected from socket server");
     };
 
-    SocketHelper.prototype.error = function(){
+    WebsocketHelper.prototype.error = function(){
         console.error("can't connect to socket server");
     };
 
