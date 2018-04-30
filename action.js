3692dfb72381afdeb410a6cb7adcbb789e37f978
Corentin Bègne <corentin.begne@gmail.com>
2018-04-30 11:33:39 +0200

    remove percentprogress action

--- action.js
+++ action.js
@@ -13,18 +13,24 @@ var ActionHelper;
         var that = this;
         extendSingleton(ActionHelper);
         this.loadingHtml = "";
-        loadCss((window.baseUrl ? window.baseUrl : "")+"/bower_components/jquery.percentageloader/index.css");
+        this.css = ".backdrop{background:rgba(0,0,0,0.3);width:100%;height:100%;z-index:999999;position:fixed;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;top:0;left:0}.backdrop #loader{position:absolute;top:calc(50% - 25px);left:calc(50% - 25px);border:16px solid #f3f3f3;border-top:16px solid #337ab7;border-radius:50%;width:50px;height:50px;animation:spin 2s linear infinite}@keyframes spin{0%{-moz-transform:rotate(0deg);-webkit-transform:rotate(0deg);-o-transform:rotate(0deg);-ms-transform:rotate(0deg);transform:rotate(0deg)}100%{-moz-transform:rotate(360deg);-webkit-transform:rotate(360deg);-o-transform:rotate(360deg);-ms-transform:rotate(360deg);transform:rotate(360deg)}}";
+        var script = $("<script></script>");
+        scrpit.text(this.css);
+        $("head").append(script);
+ /*       loadCss((window.baseUrl ? window.baseUrl : "")+"/bower_components/jquery.percentageloader/index.css");
         require([
             "bower_components/jquery-percentageloader/index"
-        ], loaded);
+        ], loaded);*/
+        if(isDefined(cb)){
+            cb(that);
+        }
         this.basePath = "/"+$("body").attr("app")+"/";
         var hasOnProgress = ("onprogress" in $.ajaxSettings.xhr());
-        if (!hasOnProgress) {
-            return;
-        }       
-        //patch ajax settings to call a progress callback
-        var oldXHR = $.ajaxSettings.xhr;
-        $.ajaxSettings.xhr = setAjaxSetting;
+        if (hasOnProgress) {   
+            //patch ajax settings to call a progress callback
+            var oldXHR = $.ajaxSettings.xhr;
+            $.ajaxSettings.xhr = setAjaxSetting;
+        }
 
         function setAjaxSetting(){
             var xhr = oldXHR();
@@ -39,11 +45,11 @@ var ActionHelper;
             return xhr;
         }
 
-        function loaded(){
+    /*    function loaded(){
             if(isDefined(cb)){
                 cb(that);
             }
-        }
+        }*/
     };
 
     /**
@@ -73,13 +79,13 @@ var ActionHelper;
                 $("body").append(div);
             } else {
                 $("body").append("<div class='backdrop'><div id='loader'></div></div>");
-                var loader = $("#loader").percentageLoader({
+               /* var loader = $("#loader").percentageLoader({
                     width : 128, 
                     height : 128, 
                     progress : 0, 
                     value : "chargement"
-                });
-                if(isDefined(options.upload)){
+                });*/
+             /*   if(isDefined(options.upload)){
                     $("body .backdrop").append("<div id='uploader'></div>");
                     var uploader = $("#uploader").percentageLoader({
                         width : 64, 
@@ -87,7 +93,7 @@ var ActionHelper;
                         progress : 0, 
                         value : "upload"
                     });
-                }
+                }*/
             }
         }
         var infos = {
@@ -113,10 +119,10 @@ var ActionHelper;
                 return false;
             }
             if(event.target instanceof XMLHttpRequest){
-                loader.setProgress(event.loaded / event.total);   
+               // loader.setProgress(event.loaded / event.total);   
             } 
             if(isDefined(options.upload) && event.target instanceof XMLHttpRequestUpload){
-                uploader.setProgress(event.loaded / event.total);    
+                //uploader.setProgress(event.loaded / event.total);    
             }       
         }
 
